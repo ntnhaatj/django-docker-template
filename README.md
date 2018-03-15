7f0ab1792bc938586c527df312f2452aadf3ddf4
ntnhaatj <ntnhat.d@gmail.com>
2018-03-16 01:50:57 +0700

    Change to gunicorn instead of uwsgi

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# django-docker-template
+# Docker template for DJango
+---------------------------------
+
+## Overview
+
+These instructions will deploy DJango system in production mode by docker.
+
+System architecture: (to be added)
+
+## Configuration
+
+### Posgresql
+(To be added)
+
+### Nginx
+(To be added)
+
+### Webapps
+(To be added)
+
+## Deployment
+
+Build docker images
+
+```
+docker-compose build
+```
+
+Deploy containers as daemon
+
+```
+docker-compose up -d
+```
+
+Show containers log
+
+```
+docker-compose logs -f
+```
--- mysite/run_webapps.sh
+++ mysite/run_webapps.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+USER=`whoami`                                     # the user to run as
+NUM_WORKERS=10
+TIMEOUT=300
+DJANGO_SETTINGS_MODULE="mysite.settings"          # which settings file should Django use
+DJANGO_WSGI_MODULE="mysite.wsgi"                  # WSGI module name
+echo "Starting as `whoami`"
+
+# waiting for database start completely
+sleep 3
 cd /webapps
 export DJANGO_SETTINGS_MODULE=mysite.settings
 python3 manage.py makemigrations
 python3 manage.py migrate
-uwsgi --ini uwsgi.ini
+
+exec gunicorn ${DJANGO_WSGI_MODULE}:application  --workers $NUM_WORKERS  --bind 0.0.0.0:8000  --timeout $TIMEOUT   --log-level=info
+
